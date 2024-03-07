ea8ae7e6d4dd061b31ada7136aea7024b23ec051
igorhls <igorhls@hotmail.com>
2024-03-07 19:31:30 -0300

    Adicionei um codigo de meu computador

---
+++ Olá Ray.py
@@ -0,0 +1,13 @@
+#Criei esse arquivo em meu computador e o enviei para o repositório do GitHub.
+print ('Olá Ray!')
+
+
+#olha só essa soma aqui em baixo.
+a = 10
+b = 20
+soma = a + b
+print(soma)
+
+#Manda um abraço pra Moises e manda ele liberar a turma de organização de computadores mais cedo.
+
+#Igor :P
